13791d08ea219f7e14dca2a456406fa9bac85b37
Tim Claes <tim.claes@me.com>
2017-10-20 21:25:23 +0200

    more project setup

---
+++ client/src/components/App.js
@@ -0,0 +1,11 @@
+import react, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        HELLO WORLD
+      </div>
+    )
+  }
+}
