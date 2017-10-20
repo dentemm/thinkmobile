5f7cd9b0576117fd1ac953000a4b82ee38486237
Tim Claes <tim.claes@me.com>
2017-10-20 21:13:26 +0200

    further boilerplate setup for frontend code

---
+++ client/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = () => {
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        }
+      ]
+    }
+  }
+}
