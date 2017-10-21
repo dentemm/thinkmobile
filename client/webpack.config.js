dbe0fa467edb2ea693621855015295a4f167d832
Tim Claes <tim.claes@me.com>
2017-10-21 20:33:51 +0200

    working on production webpack structure

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 
-module.exports = () => {
+module.exports = (env) => {
+
+  const isProduction = env === 'production';
+
   return {
     entry: './src/index.js',
     output: {
@@ -24,6 +27,7 @@ module.exports = () => {
         }
       ]
     },
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
