ad85fb5a835ef3ef08a59554174fedf35b640c09
Tim Claes <tim.claes@me.com>
2017-10-22 10:58:25 +0200

    trying to get express server to return client code

--- index.js
+++ index.js
@@ -3,9 +3,32 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hello: 'there'});
-});
+// app.get('/', (req, res) => {
+//   res.send({ hello: 'there'});
+// });
+
+// app.get('*', (req, res) => {
+//   app.use(express.static('client/dist'));
+//   const path = require('path');
+//   res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+// });
+
+const path = require('path');
+const publicPath = path.join(__dirname, 'client', 'public');
+const otherPath = path.join(__dirname, 'client', 'dist');
+app.use(express.static(publicPath));
+app.use(express.static(otherPath));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/dist'));
+
+  const path = require('path');
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+  });
+
+}
 
 PORT = process.env.PORT || 8000;
 
