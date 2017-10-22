f57099a9138729171b600bf781c37395f3f560a8
Tim Claes <tim.claes@me.com>
2017-10-22 14:27:45 +0200

    working express server

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = (env) => {
     entry: './src/index.js',
     output: {
       filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
+      path: path.join(__dirname, 'public', 'dist')
     },
     module: {
       rules: [
